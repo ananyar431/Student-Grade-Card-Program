17cf06dfc893670e291abd22a9339550d86708db
ananyar431 <165397989+ananyar431@users.noreply.github.com>
2024-03-29 20:00:50 +0530

    gradetable.h

---
+++ gradetable.h
@@ -0,0 +1,7 @@
+#ifndef GRADETABLE_H
+#define GRADETABLE_H
+
+char grade2(int n);
+void gradetable(char studentname[][50], int rollno[], int isa1[][3], int isa2[][3], int esa[][3], int assignment[][3]);
+
+#endif
