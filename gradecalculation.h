718ac59bcc5396bf0250fd89dcb5f3ee3490fd94
ananyar431 <165397989+ananyar431@users.noreply.github.com>
2024-03-29 19:59:39 +0530

    gradecalculation.h

---
+++ gradecalculation.h
@@ -0,0 +1,6 @@
+#ifndef GRADECALCULATION_H
+#define GRADECALCULATION_H
+void grade(int n, int i, int sub);
+void gradecalculation(int isa1[][3], int isa2[][3], int esa[][3], int assignment[][3]);
+
+#endif
